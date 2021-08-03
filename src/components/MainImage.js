c1fb2ea55f53b5f15bc8b9ef125d172a3d96c61e
fadelaf <fadelahmadf@gmail.com>
2021-08-04 00:18:21 +0700

    perbaikan img

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import image from '../img/mainimage.jpg'
+// import image from '../img/mainimage.jpg'
 const MainImage = () => {
 
     return (
         <div className="container main-image">
-            <img src={image} alt=""></img>
+            <img src="https://images.unsplash.com/photo-1480796927426-f609979314bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt=""></img>
         </div>
             )
 }
